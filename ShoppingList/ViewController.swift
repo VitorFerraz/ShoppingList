832a744ea47c13cd1c29da174796779b1588ee09
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-05-03 13:48:15 -0300

    Added SwiftUI Preview and disabled iPad and landspace mode

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        
     }
 
 
 }
 
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct ViewRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        return ViewController().view!
+    }
+    
+    func updateUIView(_ view: UIView, context: Context) {
+        
+    }
+}
+
+@available(iOS 13.0, *)
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ViewRepresentable()
+    }
+}
+#endif
